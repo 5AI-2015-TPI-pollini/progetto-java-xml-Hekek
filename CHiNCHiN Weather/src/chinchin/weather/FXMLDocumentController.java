c50248b1ff26666630bc76b2c2d1ca9eeda62150
Mosta <Mosta@Mosta-PC>
2015-11-21 01:27:39 -0800

    - Added non Working Proxy Settings - Added XPath Reader - Added google status Test

--- CHiNCHiN Weather/src/chinchin/weather/FXMLDocumentController.java
+++ CHiNCHiN Weather/src/chinchin/weather/FXMLDocumentController.java
@@ -5,12 +5,33 @@
  */
 package chinchin.weather;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -29,7 +50,63 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        try {
+            
+           /* System.setProperty("proxySet", "true");
+            System.setProperty("http.proxyHost", "192.168.0.1");
+            System.setProperty("http.proxyPort", "8080");
+            Authenticator.setDefault(new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    
+                    return new PasswordAuthentication("19101229","Juventus1".toCharArray());
+                }
+            });*/
+            
+            URL gurl = new URL("http://maps.googleapis.com/maps/api/geocode/xml?address=brescia");
+            URLConnection con = gurl.openConnection();
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            // Read it ...
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                System.out.println(inputLine);
+            
+            
+            
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse("http://maps.googleapis.com/maps/api/geocode/xml?address=brescia");
+            XPathFactory xPathfactory = XPathFactory.newInstance();
+            XPath xpath = xPathfactory.newXPath();
+           
+            String status = getStatus(doc, xpath);
+            
+            System.out.println(status);
+            
+            in.close();
+            
+              
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (XPathExpressionException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+   
+    }
+    
+    private static String getStatus (Document doc , XPath xpath) throws XPathExpressionException{
+        XPathExpression expr = xpath.compile("/GeocodeResponse/status");
+        return (String) expr.evaluate(doc, XPathConstants.STRING);
+        
+    }
     
 }
+
